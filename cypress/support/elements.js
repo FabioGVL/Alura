5c2d4a4223fbc5900c9179d0bac1b24c2ffd82b0
FabioGVL <fabio.bytecode@gmail.com>
2023-06-29 04:03:08 -0300

    Cadastro Alura Automation

---
+++ cypress/support/elements.js
@@ -0,0 +1,17 @@
+const elements = {
+    
+    //ID's correspondentes aos campos de login, usuário, senha e botão registrar/logar
+    
+        campoEmailCadastro: ':nth-child(1) > .form-control',
+        campoNomeUsuarioCadastro: ':nth-child(2) > .form-control',
+        campoUserNameCadastro: ':nth-child(3) > .form-control',
+        campoSenhaCadastro: ':nth-child(4) > .form-control',
+        campoUserNameLogin:':nth-child(1) > .form-control',
+        campoSenhaLogin:':nth-child(2) > .form-control',
+        botaoRegistrar: '.btn',
+        botaoLogin: '.btn'
+
+    
+
+}
+export default elements;
